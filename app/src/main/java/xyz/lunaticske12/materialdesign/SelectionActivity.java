65b93bb3f914c1df1ad80a4c362535cc2cf9126b
lunaticSKE12 <jamesnapongd@gmail.com>
2017-09-19 01:09:23 +0700

    Group Radio button

--- app/src/main/java/xyz/lunaticske12/materialdesign/SelectionActivity.java
+++ app/src/main/java/xyz/lunaticske12/materialdesign/SelectionActivity.java
@@ -1,15 +1,21 @@
 package xyz.lunaticske12.materialdesign;
 
 import android.provider.Settings;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatRadioButton;
 import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class SelectionActivity extends AppCompatActivity {
 
     AppCompatCheckBox appCompatCheckBox;
+    AppCompatRadioButton appCompatRadioButton;
+
+    RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,32 @@ public class SelectionActivity extends AppCompatActivity {
                 else {
                     Toast.makeText(getApplicationContext(), "CheckBox is Disable", Toast.LENGTH_SHORT).show();
 
+                }
+            }
+        });
+
+        appCompatRadioButton = (AppCompatRadioButton) findViewById(R.id.RadioButton1_id);
+        appCompatRadioButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup_id);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+
+                switch (checkedId){
+                    case R.id.RadioButton1_id:
+                        Toast.makeText(getApplicationContext(), "Check 1", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.RadioButton2_id:
+                        Toast.makeText(getApplicationContext(), "Check 2", Toast.LENGTH_SHORT).show();
+                        break;
                 }
             }
         });
